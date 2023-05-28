cc33c14bee9c36a4afadd0b2ac55bdc5aa336b18
Christiaan Bloemendaal <github@tnrd.net>
2023-05-28 16:16:48 +0200

    feat: initial version

---
+++ MediaQueue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using FluentResults;
+using TNRD.Zeepkist.GTR.DTOs.Rabbit;
+
+namespace TNRD.Zeepkist.GTR.Backend.RecordMediaHandler;
+
+internal class MediaQueue
+{
+    private readonly AutoResetEvent resetEvent = new(true);
+    private readonly ConcurrentQueue<UploadRecordMediaRequest> items = new();
+
+    public bool HasItems()
+    {
+        return !items.IsEmpty;
+    }
+
+    public void AddToQueue(UploadRecordMediaRequest item)
+    {
+        resetEvent.WaitOne();
+        items.Enqueue(item);
+        resetEvent.Set();
+    }
+
+    public Result<UploadRecordMediaRequest> GetItemFromQueue()
+    {
+        resetEvent.WaitOne();
+        Result<UploadRecordMediaRequest> result = items.TryDequeue(out UploadRecordMediaRequest? item)
+            ? Result.Ok(item)
+            : Result.Fail("Failed to dequeue");
+        resetEvent.Set();
+        return result;
+    }
+}
